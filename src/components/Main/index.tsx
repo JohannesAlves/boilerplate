5f09d6fe87b5e3b3fed0a5aae65cf38bc7c68fde
JohannesAlves <johannes001002@gmail.com>
2023-01-06 16:16:16 -0300

    chore: try first test

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,4 @@
 export default function Main() {
-  const test = ""
-  const test2 = ""
-  const test2 = "duvido pasa"
-
   return (
     <main>
       <h1>react avançado</h1>
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -7,7 +7,7 @@ describe("<Main/>", () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole("heading", { name: /reacteee avançado/i })
+      screen.getByRole("heading", { name: /react avançado/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
